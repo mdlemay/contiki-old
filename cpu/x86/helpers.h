59b2520140b26430a5043ddfbcc57a4408dbfd8a
Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
2015-04-08 17:18:27 -0300

    x86: Add outb helper function
    This function is a wrapper for the out assembly instruction.

--- cpu/x86/helpers.h
+++ cpu/x86/helpers.h
@@ -1,8 +1,17 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#include <stdint.h>
+
+
 #define BIT(n) (1 << (n))
 
 void halt(void) __attribute__((__noreturn__));
+
+/**
+ * Wrapper for the assembly 'out' instruction.
+ *
+ */
+void outb(uint16_t port, uint8_t byte);
 
 #endif
