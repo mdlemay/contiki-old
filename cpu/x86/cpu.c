e586de80773f522ce85898608fd09eb71bbd23c9
Andre Guedes <andre.guedes@intel.com>
2015-04-15 17:44:38 -0300

    x86: Set interrupt handler for Double Fault exception
    This patch sets an interrupt handler for Double Fault exception during
    CPU initialization. In case such exception is raised, we halt the
    system.
    
    This way, we avoid the system to triple fault (due to an unhandled
    interrupt for instance), leaving no trace about what cause the triple
    fault.
    
    This patch also fixes wrong indentation in cpu_init() function.

--- cpu/x86/cpu.c
+++ cpu/x86/cpu.c
@@ -1,9 +1,21 @@
 #include "gdt.h"
 #include "idt.h"
+#include "interrupt.h"
+#include "helpers.h"
+
+void double_fault_handler(struct interrupt_context context)
+{
+  halt();
+}
 
 void
 cpu_init(void)
 {
- gdt_init();
- idt_init();
+  gdt_init();
+  idt_init();
+
+  /* Set an interrupt handler for Double Fault exception. This way, we avoid
+   * the system to triple fault, leaving no trace about what happened.
+   */
+  SET_INTERRUPT_HANDLER(8, 1, double_fault_handler);
 }
