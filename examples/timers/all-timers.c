b6b27e020998fc997bcb464976b3503605a9cb39
Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
2015-04-22 15:47:21 -0300

    examples: Add all-timers example
    This commit adds a very simple example which is useful to verify
    that all timers APIs are working. There are 2 protothreads running,
    the first process tests etimer, timer and stimer APIs. The second process
    tests the ctimer APIs.
    
    This example has been tested with both native and galileo ports.

---
+++ examples/timers/all-timers.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#include "contiki.h"
+#include "sys/etimer.h"
+#include "sys/stimer.h"
+#include "sys/timer.h"
+
+PROCESS(process1, "ETimer x Timer x STimer Process");
+PROCESS(process2, "CTimer Process 2");
+AUTOSTART_PROCESSES(&process1, &process2);
+
+static int counter_etimer;
+static int counter_timer;
+static int counter_stimer;
+static int counter_ctimer;
+static struct timer timer_timer;
+static struct stimer timer_stimer;
+static struct ctimer timer_ctimer;
+
+void do_timeout1()
+{
+  counter_etimer++;
+  if (timer_expired(&timer_timer)) {
+    counter_timer++;
+  }
+
+  if (stimer_expired(&timer_stimer)) {
+    counter_stimer++;
+  }
+
+  printf("\nProcess 1: %s", counter_timer == counter_etimer
+                            && counter_timer == counter_stimer ? "SUCCESS" : "FAIL");
+}
+
+void do_timeout2()
+{
+  ctimer_reset(&timer_ctimer);
+  printf("\nProcess 2: CTimer callback called");
+  counter_ctimer++;
+}
+
+
+PROCESS_THREAD(process1, ev, data)
+{
+    static struct etimer timer_etimer;
+
+    PROCESS_BEGIN();
+
+    while (1) {
+      timer_set(&timer_timer, 3*CLOCK_SECOND);
+      stimer_set(&timer_stimer, 3);
+      etimer_set(&timer_etimer, 3*CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+      do_timeout1();
+    }
+
+    PROCESS_END();
+}
+
+PROCESS_THREAD(process2, ev, data)
+{
+    PROCESS_BEGIN();
+
+    while (1) {
+      ctimer_set(&timer_ctimer, 5*CLOCK_SECOND, do_timeout2, NULL);
+      PROCESS_YIELD();
+    }
+
+    PROCESS_END();
+}
+
+
