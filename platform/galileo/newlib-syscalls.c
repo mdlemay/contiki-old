e9fe54fbdab520e2d80e9f692ce22e72640c0dd2
Andre Guedes <andre.guedes@intel.com>
2015-05-13 10:59:00 -0300

    galileo: Remove unneeded syscall stubs
    This patch removes _kill_r and _getpid_r stubs since they are not
    required by newlib at this moment. These stubs should not be in
    the commit that introduced the initial newlib-syscalls.c file.
    
    Since this patch touches the newlib-syscalls.c file, it also
    fixes the coding style issues reported by tools/code-style/
    uncrustify-fix-style.sh.

--- platform/galileo/newlib-syscalls.c
+++ platform/galileo/newlib-syscalls.c
@@ -1,58 +1,53 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-// Below there all needed syscall stubs so far needed by newlib.
+/* Below there all needed syscall stubs so far needed by newlib. */
 
-int _close_r(struct _reent *ptr, int file)
+int
+_close_r(struct _reent *ptr, int file)
 {
-    errno = ENOTSUP;
-    return -1;
+  errno = ENOTSUP;
+  return -1;
 }
-
-int _isatty_r(struct _reent *ptr, int file)
+/*---------------------------------------------------------------------------*/
+int
+_isatty_r(struct _reent *ptr, int file)
 {
-    errno = ENOTSUP;
-    return 0;
+  errno = ENOTSUP;
+  return 0;
 }
-
-int _read_r(struct _reent *ptr, int file, char *buf, int len)
+/*---------------------------------------------------------------------------*/
+int
+_read_r(struct _reent *ptr, int file, char *buf, int len)
 {
-    errno = ENOTSUP;
-    return 0;
+  errno = ENOTSUP;
+  return 0;
 }
-
-int _write_r(struct _reent *ptr, int file, const char *buf, int len)
+/*---------------------------------------------------------------------------*/
+int
+_write_r(struct _reent *ptr, int file, const char *buf, int len)
 {
-    errno = ENOTSUP;
-    return -1;
+  errno = ENOTSUP;
+  return -1;
 }
-
-int _lseek_r(struct _reent *ptr, int file, int p, int dir)
+/*---------------------------------------------------------------------------*/
+int
+_lseek_r(struct _reent *ptr, int file, int p, int dir)
 {
-    errno = ENOTSUP;
-    return 0;
+  errno = ENOTSUP;
+  return 0;
 }
-
-int _fstat_r(struct _reent *ptr, int file, struct stat *st)
+/*---------------------------------------------------------------------------*/
+int
+_fstat_r(struct _reent *ptr, int file, struct stat *st)
 {
-    errno = ENOTSUP;
-    return -1;
+  errno = ENOTSUP;
+  return -1;
 }
-
-caddr_t _sbrk_r(struct _reent *ptr,int incr)
+/*---------------------------------------------------------------------------*/
+caddr_t
+_sbrk_r(struct _reent *ptr, int incr)
 {
-    errno = ENOTSUP;
-    return NULL;
+  errno = ENOTSUP;
+  return NULL;
 }
-
-void _kill_r(struct _reent *ptr)
-{
-    errno = ENOTSUP;
-}
-
-int _getpid_r(struct _reent *ptr)
-{
-    errno = ENOTSUP;
-    return 1;
-}
-
