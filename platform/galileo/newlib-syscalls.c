212a22537e9696f57e1e28639421fa22486c0b60
Andre Guedes <andre.guedes@intel.com>
2015-06-03 19:47:33 -0300

    galileo: Fix newlib stub
    This patch fixes some issues related to newlib stubs from Galileo
    platforms.
    
    The stubs implement the reentrant version of system calls so they
    should have been using the errno variable within the struct reent
    instead of the global one.
    
    That change has spotted another issue related to the library order in
    TARGET_LIBFILES variable. The linker is reporting the following error:
    
    ../../platform/galileo/bsp/i586-elf/lib/libm.a(lib_a-w_log.o): In function `log':
    w_log.c:(.text+0xd2): undefined reference to `__errno'
    w_log.c:(.text+0xfe): undefined reference to `__errno'
    w_log.c:(.text+0x22): undefined reference to `__errno'
    
    The issue is that libm needs symbols from libc therefore it should
    come ahead in the library order, but it doesn't. For more information
    about library order in static linking see [1].
    
    [1] http://eli.thegreenplace.net/2013/07/09/library-order-in-static-linking

--- platform/galileo/newlib-syscalls.c
+++ platform/galileo/newlib-syscalls.c
@@ -6,48 +6,48 @@
 int
 _close_r(struct _reent *ptr, int file)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return -1;
 }
 /*---------------------------------------------------------------------------*/
 int
 _isatty_r(struct _reent *ptr, int file)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return 0;
 }
 /*---------------------------------------------------------------------------*/
 int
 _read_r(struct _reent *ptr, int file, char *buf, int len)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return 0;
 }
 /*---------------------------------------------------------------------------*/
 int
 _write_r(struct _reent *ptr, int file, const char *buf, int len)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return -1;
 }
 /*---------------------------------------------------------------------------*/
 int
 _lseek_r(struct _reent *ptr, int file, int p, int dir)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return 0;
 }
 /*---------------------------------------------------------------------------*/
 int
 _fstat_r(struct _reent *ptr, int file, struct stat *st)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return -1;
 }
 /*---------------------------------------------------------------------------*/
 caddr_t
 _sbrk_r(struct _reent *ptr, int incr)
 {
-  errno = ENOTSUP;
+  ptr->_errno = ENOTSUP;
   return NULL;
 }
